d011d530c454d211bb2089056ae9b8f5b8c25bde
Yao Serge Eric KOUASSI <sergeerickouassi@icloud.com>
2025-03-24 07:37:18 +0100

    Add method to exposed globaly

--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ root.render(
   </React.StrictMode>
 );
 
+// Function to initialize the React app in a specific container
+window.initializeReactApp = (container) => {
+  // Check if the container is a valid DOM element
+  if (!(container instanceof Element)) {
+    console.error("Invalid container provided to initializeReactApp. It must be a DOM element.");
+    return;
+  }
+  
+  // Use ReactDOM.createRoot for React 18+
+  const customRoot = ReactDOM.createRoot(container);
+  customRoot.render(<App />);
+};
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
